fc690f475d88917cf857f5a929d564c31945ee50
lexi.callyscoped <lexi.callyscoped@gmail.com>
2024-11-10 22:17:20 +0530

    build: add rosenthal, fix guix to commit

--- channels.lock.scm
+++ channels.lock.scm
@@ -3,18 +3,29 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "ad966c3844385e8f7b60b0dc915f92a1eba47e09")
+          "3b78eca656cf8a088ca8699a0563e67e2b61f2ac")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
+        (name 'rosenthal)
+        (url "https://codeberg.org/hako/rosenthal.git")
+        (branch "trunk")
+        (commit
+          "d219e6e52062bea953116763dd1fa602942e2e24")
+        (introduction
+          (make-channel-introduction
+            "7677db76330121a901604dfbad19077893865f35"
+            (openpgp-fingerprint
+              "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "bd26815cf8ce38a3b03676a6e3fc482bb74247cb")
+          "4c56d0cccdc44e12484b26332715f54768738c5f")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
