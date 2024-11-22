3cc8fd944e8c700f8c2424a5cc3c8646b64bc492
lexi.callyscoped <lexi.callyscoped@gmail.com>
2024-11-22 15:31:36 +0530

    refactor: generic system module + explicit use-module

--- aetheria.scm
+++ aetheria.scm
@@ -1,5 +1,5 @@
 (define-module (aetheria)
-  #:use-module (gnu system) ;; operating-system?
+  #:use-module ((gnu system) #:select (operating-system?))
   #:export (%project-root
             config-for-os))
 
@@ -9,21 +9,8 @@
        "/projects/aetheria"))
 
 (define* (config-for-os #:optional (hostname (gethostname)))
-  (define module-name `(aetheria hosts ,(string->symbol hostname)))
-  (define os-var-name (string->symbol (string-append hostname "-operating-system")))
-
-  (define host-module
-    (resolve-module module-name #t #:ensure #f))
-  (unless host-module
-    (error (format #f "configuration not found. ensure that module ~a exists"
-                   module-name)))
-  (define host-os-var
-    (module-variable host-module os-var-name))
-  (unless host-os-var
-    (error (format #f "module ~a exists, but variable '~a' doesn't exist"
-                   module-name os-var-name)))
-  (define host-os (variable-ref host-os-var))
-  (unless (operating-system? host-os)
-    (error (format #f "module ~a has variable '~a' but isn't of type <operating-system>"
-                   module-name os-var-name)))
-  host-os)
+  (define host-file-name (string-append "aetheria/hosts/" hostname ".scm"))
+  (define os (primitive-load-path host-file-name)) ;; the default error is good enough
+  (unless (operating-system? os)
+    (error "file ~a doesn't produce an <operating-system>" host-file-name))
+  os)
