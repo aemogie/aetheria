a61f00322f6aec22c1fd5fbd7b7bdb6cc17fa0ba
lexi.callyscoped <lexi.callyscoped@gmail.com>
2024-11-23 13:04:29 +0530

    refactor: base home environment

---
+++ aetheria/home/base.scm
@@ -0,0 +1,63 @@
+(define-module (aetheria home base)
+  #:use-module ((gnu services) #:select (service))
+  #:use-module ((gnu system shadow) #:select (%default-dotguile
+                                              %default-xdefaults
+                                              %default-gdbinit
+                                              %default-nanorc))
+  #:use-module ((gnu home) #:select (home-environment))
+  #:use-module ((gnu home services) #:select (home-files-service-type
+                                              home-xdg-configuration-files-service-type))
+  #:use-module ((gnu home services shepherd) #:select (home-shepherd-service-type))
+  #:use-module ((gnu home services shells) #:select (home-bash-service-type))
+  #:use-module ((gnu home services desktop) #:select (home-dbus-service-type))
+  #:use-module ((gnu home services sound) #:select (home-pipewire-service-type))
+  #:use-module ((gnu packages base) #:select (gnu-make))
+  #:use-module ((gnu packages gcc) #:select (gcc))
+  #:use-module ((gnu packages text-editors) #:select (nano))
+  #:use-module ((gnu packages vim) #:select (vim))
+  #:use-module ((gnu packages emacs) #:select (emacs-next-pgtk-xwidgets))
+  #:export (%aetheria-base-home-services
+            %aetheria-base-home-packages
+            %aetheria-desktop-home-services
+            %aetheria-desktop-home-packages
+            %aetheria-desktop-home))
+
+(define %aetheria-base-home-services
+  (list
+   (service home-bash-service-type)
+   ;; runs weirdly. C-c kills it for some reason?
+   (service home-shepherd-service-type)
+   (service home-files-service-type
+            `((".guile" ,%default-dotguile)
+              (".Xdefaults" ,%default-xdefaults)))
+
+   (service home-xdg-configuration-files-service-type
+            `(("gdb/gdbinit" ,%default-gdbinit)
+              ("nano/nanorc" ,%default-nanorc)))))
+
+(define %aetheria-base-home-packages
+  (list gnu-make ;; make for building the system
+        gcc ;; always have a c compiler too
+        ;; and always have a couple editors i dont touch the config of
+        nano vim))
+
+(define %aetheria-base-home
+  (home-environment
+   (services %aetheria-base-home-services)
+   (packages %aetheria-base-home-packages)))
+
+(define %aetheria-desktop-home-services
+  (cons*
+   (service home-dbus-service-type)
+   (service home-pipewire-service-type)
+   %aetheria-base-home-services))
+
+(define %aetheria-desktop-home-packages
+  (cons*
+   emacs-next-pgtk-xwidgets ;; TODO: emacs module?
+   %aetheria-base-home-packages))
+
+(define %aetheria-desktop-home
+  (home-environment
+   (services %aetheria-desktop-home-services)
+   (packages %aetheria-desktop-home-packages)))
