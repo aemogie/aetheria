77f310a8d38a23f65295cfdffc50a3b19a4a2ecf
aemogie. <theaemogie@gmail.com>
2024-11-27 22:37:31 +0530

    feat: channels in repl, finally

--- aetheria/home/base.scm
+++ aetheria/home/base.scm
@@ -1,4 +1,7 @@
 (define-module (aetheria home base)
+  #:use-module ((guix gexp) #:select (gexp
+                                      plain-file
+                                      scheme-file))
   #:use-module ((gnu services) #:select (service))
   #:use-module ((gnu system shadow) #:select (%default-dotguile
                                               %default-xdefaults
@@ -18,7 +21,6 @@
   #:use-module ((gnu packages text-editors) #:select (nano))
   #:use-module ((gnu packages vim) #:select (vim))
   #:use-module ((gnu packages emacs) #:select (emacs-pgtk-xwidgets))
-  #:use-module ((guix gexp) #:select (plain-file))
   #:use-module ((gnu packages xdisorg) #:select (wl-clipboard))
   #:use-module ((gnu packages terminals) #:select (foot))
   #:use-module ((gnu packages fonts) #:select (font-iosevka
@@ -35,13 +37,46 @@
             %aetheria-desktop-home-packages
             %aetheria-desktop-home))
 
+(define dotguile-gexp
+  #~(;; default .guile
+     (cond ((false-if-exception (resolve-interface '(ice-9 readline)))
+            =>
+            (lambda (module)
+              ;; Enable completion and input history at the REPL.
+              ((module-ref module 'activate-readline))))
+           (else
+            (display "Consider installing the 'guile-readline' package for
+convenient interactive line editing and input history.\n\n")))
+
+     (unless (getenv "INSIDE_EMACS")
+       (cond ((false-if-exception (resolve-interface '(ice-9 colorized)))
+              =>
+              (lambda (module)
+                ;; Enable completion and input history at the REPL.
+                ((module-ref module 'activate-colorized))))
+             (else
+              (display "Consider installing the 'guile-colorized' package
+for a colorful Guile experience.\n\n"))))
+     ;; end of default .guile
+
+     ;; do we just pray these exist?
+     (use-modules ((guix inferior) #:select (cached-channel-instance))
+                  ((guix store) #:select (with-store)))
+
+     (define channel-list (@@(guix scripts time-machine) channel-list))
+     (define cached-inferior
+       (with-store store (cached-channel-instance store (channel-list '()))))
+     (define inferior-guile-path
+       (string-append cached-inferior "/share/guile/site/3.0"))
+     (add-to-load-path inferior-guile-path)))
+
 (define %aetheria-base-home-services
   (list
    (service home-bash-service-type)
    ;; started from hyprland config which is being persisted locally for now
    (service home-shepherd-service-type (home-shepherd-configuration (auto-start? #f)))
    (service home-files-service-type
-            `((".guile" ,%default-dotguile)
+            `((".guile" ,(scheme-file "dotguile" dotguile-gexp #:splice? #t))
               (".Xdefaults" ,%default-xdefaults)))
 
    (service home-xdg-configuration-files-service-type
