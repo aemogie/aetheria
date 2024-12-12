cfa1e4c6080eb3c60f97d8961aeb60db67774656
aemogie. <theaemogie@gmail.com>
2024-12-12 23:07:26 +0530

    docs: README and TODO

--- .dir-locals.el
+++ .dir-locals.el
@@ -28,7 +28,6 @@
    (eval . (put 'match-record 'scheme-indent-function 3))
    (eval . (put 'match-record-lambda 'scheme-indent-function 2))
 
-   ;; TODO: Contribute these to Emacs' scheme-mode.
    (eval . (put 'let-keywords 'scheme-indent-function 3))
 
    ;; 'modify-inputs' and its keywords.
