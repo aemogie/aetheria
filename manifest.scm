11fb47b497b68a51ec8ac732537f9a08d88c12a0
aemogie. <theaemogie@gmail.com>
2025-01-05 17:48:50 +0530

    chore: change the file that matters the least

--- manifest.scm
+++ manifest.scm
@@ -1,6 +1,5 @@
 (use-modules
  ((guix profiles) #:select (packages->manifest))
- ((gnu packages guile) #:select (guile-3.0))
  ((gnu packages base) #:select (gnu-make)))
 
-(packages->manifest (list guile-3.0 gnu-make))
+(packages->manifest (list gnu-make))
