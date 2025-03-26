968bc5921c36033acc2a1b5c9f9088de142603ae
aemogie. <theaemogie@gmail.com>
2025-03-27 01:47:57 +0530

    feat: add typst from guix-science channel

--- channels.lock.scm
+++ channels.lock.scm
@@ -20,6 +20,17 @@
             (openpgp-fingerprint
               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
+        (name 'guix-science)
+        (url "https://codeberg.org/guix-science/guix-science.git")
+        (branch "master")
+        (commit
+          "b3093f210962295957b6a3a7e37dcfc2d481d17f")
+        (introduction
+          (make-channel-introduction
+            "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
