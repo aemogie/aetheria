d73ca83deb00ecab71a9ed56d899b88ded581eb8
aemogie. <theaemogie@gmail.com>
2025-04-08 02:14:17 +0530

    refactor: wip broken
    its 2am i cant

--- aetheria/home/services/security.scm
+++ aetheria/home/services/security.scm
@@ -29,7 +29,8 @@
                                                pinentry
                                                pinentry-tty))
   #:use-module ((gnu packages ssh) #:select (openssh-sans-x))
-  #:use-module ((aetheria records) #:select (define-foldable-record-type))
+  #:use-module ((aetheria records) #:select (define-foldable-record-type
+                                              define-foldable-wrapper-type))
   #:export (home-openssh-service-type
             home-openssh-configuration
             home-openssh-configuration-authorized-keys
@@ -44,39 +45,34 @@
   (let ((xs* (concatenate (map proc xs))))
     (if (pair? xs*) xs* on-empty)))
 
-(define-foldable-record-type <home-openssh-configuration>
-  home-openssh-configuration make-home-openssh-configuration
-  home-openssh-configuration? fold-home-openssh-configuration
-  (authorized-keys   home-openssh-configuration-authorized-keys ;list of file-like
-                     (fold list))
-  (known-hosts       home-openssh-configuration-known-hosts ;list of file-like
-                     (fold list))
-  (hosts             home-openssh-configuration-hosts ;list of <openssh-host>
-                     (fold list))
-  (add-keys-to-agent home-openssh-configuration-add-keys-to-agent ;string with limited values
-                     (fold conflict)))
-
+(define-foldable-wrapper-type
+  home-openssh-configuration upstream:home-openssh-configuration
+  (%default (upstream:home-openssh-configuration))
+  (authorized-keys list)
+  (known-hosts list)
+  (hosts list)
+  (add-keys-to-agent conflict))
 
 ;; TOOD: make another macro wrapper on top of foldable record type to make
 ;; this easier
-(define home-openssh-configuration->upstream
-  (match-lambda
-    (($ <home-openssh-configuration>
-        authorized-keys known-hosts hosts add-keys-to-agent)
-     (define default (upstream:home-openssh-configuration))
-     (upstream:home-openssh-configuration
-      (authorized-keys   (if (null? authorized-keys)
-                             (upstream:home-openssh-configuration-authorized-keys default)
-                             authorized-keys))
-      (known-hosts       (if (null? known-hosts)
-                             (upstream:home-openssh-configuration-known-hosts default)
-                             known-hosts))
-      (hosts             (if (null? hosts)
-                             (upstream:home-openssh-configuration-hosts default)
-                             hosts))
-      (add-keys-to-agent (if (unspecified? add-keys-to-agent)
-                             (upstream:home-openssh-configuration-add-keys-to-agent default)
-                             add-keys-to-agent))))))
+;; (define home-openssh-configuration->upstream
+;;   (match-lambda
+;;     (($ <home-openssh-configuration>
+;;         authorized-keys known-hosts hosts add-keys-to-agent)
+;;      (define default (upstream:home-openssh-configuration))
+;;      (upstream:home-openssh-configuration
+;;       (authorized-keys   (if (null? authorized-keys)
+;;                              (upstream:home-openssh-configuration-authorized-keys default)
+;;                              authorized-keys))
+;;       (known-hosts       (if (null? known-hosts)
+;;                              (upstream:home-openssh-configuration-known-hosts default)
+;;                              known-hosts))
+;;       (hosts             (if (null? hosts)
+;;                              (upstream:home-openssh-configuration-hosts default)
+;;                              hosts))
+;;       (add-keys-to-agent (if (unspecified? add-keys-to-agent)
+;;                              (upstream:home-openssh-configuration-add-keys-to-agent default)
+;;                              add-keys-to-agent))))))
 
 
 (define home-openssh-service-type
@@ -84,8 +80,8 @@
    (inherit upstream:home-openssh-service-type)
    (compose identity)
    (extend (lambda (config extensions)
-             (home-openssh-configuration->upstream
-              (fold-home-openssh-configuration extensions config))))
+             (unwrap-home-openssh-configuration
+              (fold-home-openssh-configuration (cons extensions config)))))
    (default-value (home-openssh-configuration))))
 
 
